978b2f16c844f7f8b497849c342d7163d5e956d4
Olha Hlova <olyastef@gmail.com>
2019-10-10 15:22:39 +0300

    Sample calc

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Calc
+const display = document.querySelector('.calc .display');
+document.querySelectorAll('.calc .buttons button')
+    .forEach( btn => btn.addEventListener('click', btnClicked));
+
+function btnClicked(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelector('.calc .eq').addEventListener('click' ,
+  () => display.value = eval(display.value) );
+
